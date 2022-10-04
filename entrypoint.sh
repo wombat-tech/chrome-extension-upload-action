9773b175ab34c1a2c324ce6616e0271676b402ba
dimi-sw <dmitrii.idrisov@spielworks.com>
2022-10-04 10:16:33 +0200

    Made the upload script more verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,10 +19,13 @@ jq -r '.access_token'`
 
 if [[ "$(printf '%s' "$token")" == '' ]]
 then
+  echo "Action failed during authorisation"
   exit 1
+else
+  echo "Authorisation is successful, token: $token"
 fi
 
-status=`curl \
+uploadResult=`curl \
 --silent \
 --show-error \
 --fail \
@@ -30,18 +33,23 @@ status=`curl \
 -H "x-goog-api-version: 2" \
 -X PUT \
 -T $4 \
--v https://www.googleapis.com/upload/chromewebstore/v1.1/items/$5 \
-| \
-jq -r '.uploadState'`
+-v https://www.googleapis.com/upload/chromewebstore/v1.1/items/$5`
+
+echo "Uload result: $uploadResult"
+
+status=`echo $uploadResult | jq -r '.uploadState'`
+
+echo "Uload status: $status"
 
 if [[ $status == 'FAILURE' ]]
 then
+  echo "Action failed while uploading the build"
   exit 1
 fi
 
 if [ $6 == true ] #publish
 then
-  publish=`curl \
+  publishResult=`curl \
   --silent \
   --show-error \
   --fail \
@@ -50,14 +58,20 @@ then
   -X POST \
   -T $4 \
   -v https://www.googleapis.com/upload/chromewebstore/v1.1/items/$5/publish \
-  -d publishTarget=default \
-  | \
-  jq -r '.publishState'`
+  -d publishTarget=default`
+  
+  echo "Publish result: $publishResult"
+
+  publish=`echo $publishResult | jq -r '.publishState'`
+
+  echo "Publish status: $publish"
 
   if [[ $publish == 'FAILURE' ]]
   then
+    echo "Action failed while publishing"
     exit 1
   fi
 fi
 
+echo "Upload is successful"
 exit 0
